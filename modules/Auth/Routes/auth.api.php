ebda000e4857dc0eb743de7f80170bf9d985e005
Christian D'Aquino <christian.daquino@gmail.com>
2024-08-26 21:44:16 +0800

    Update auth.api.php

--- modules/Auth/Routes/auth.api.php
+++ modules/Auth/Routes/auth.api.php
@@ -4,9 +4,11 @@ use Modules\Auth\Http\Controllers\UserController;
 
 Route::prefix('v1')->group(function () {
     Route::post('login', [UserController::class, 'login']);
-    Route::get('users/me', [UserController::class, 'me']);
-    Route::apiResource('users', UserController::class);
     Route::post('users/forgot-password', [UserController::class, 'forgotPassword']);
     Route::post('users/reset-password', [UserController::class, 'resetPassword']);
     Route::post('logout', [UserController::class, 'logout']);
+
+    Route::get('users/me', [UserController::class, 'me']);
+    Route::apiResource('users', UserController::class);
+
 });
